7ae2ad8634386bc905b989a375f4960c5f8eed3c
Luisa_Cadena <luicadenav@gmail.com>
2024-12-17 23:21:20 -0500

    changed language messages yup and required comfirm pass

--- src/utils/validateForm.ts
+++ src/utils/validateForm.ts
@@ -1,24 +1,26 @@
 import * as yup from "yup";
 
 export const LoginValidate = yup.object().shape({
-  username: yup.string().trim().email().required("El username es requerido"),
+  username: yup.string().trim().email().required("Email is required"),
   password: yup
     .string()
     .trim()
-    .required("El password es requerido")
-    .min(4, "el mínimo son son 4 caracteres")
-    .max(20, "el máximo son son 4 caracteres"),
+    .required("Password is required")
+    .min(4, "The password must be at least 4 characters long.")
+    .max(20, "The password must be no more than 4 characters long"),
 });
 
 export const RegisterValidate = yup.object().shape({
-  username: yup.string().trim().email().required("El username es requerido"),
+  username: yup.string().trim().email().required("Email is required"),
   password: yup
     .string()
     .trim()
-    .required("El password es requerido")
-    .min(4, "el mínimo son son 4 caracteres")
-    .max(20, "el máximo son son 4 caracteres"),
+    .required("Password is required")
+    .min(4, "The password must be at least 4 characters long.")
+    .max(20, "The password must be no more than 4 characters long"),
   confirmPassword: yup
     .string()
-    .oneOf([yup.ref("password")], "Passwords must match"),
+    .oneOf([yup.ref("password")], "Passwords must match")
+    .trim()
+    .required("Password is required"),
 });
